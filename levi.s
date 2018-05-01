69a2268a0408d47c0a13ed66bc0830346aff1e3c
jillyempey <jeempey@calpoly.edu>
2018-04-30 21:07:47 -0700

    Update levi.s

--- levi.s
+++ levi.s
@@ -1,4 +1,4 @@
-    /* This function has 5 parameters, and the declaration in the
+      /* This function has 5 parameters, and the declaration in the
        C-language would look like:
 
        void matadd (int **C, int **A, int **B, int height, int width)
@@ -28,39 +28,36 @@
 	.syntax unified
 	.arm
 matadd:
-    push {r4, r5, r6, r7, lr} // r4=i, r5=j, r6,r7=temp
-    mov r4, #-1
+    push {r4, r5, r6, r7, r8, lr} // r4=i, r5=j, r6r7=temp, r8=4
+    mov r8, #4
+    mov r4, #-4
 iloop:
-    add r4, r4, #1
-    cmp r4, r3 // Compare i with height-1
+    add r4, r4, #4
+    mul r7, r3, r8
+    cmp r7, r4 // Compare i with height
     beq return
-    mov r5, #-1
+    mov r5, #0
 jloop:
     ldr r6, [sp, #24]
-    add r5, r5, #1
-    cmp r5, r6 // Compare j with width
+    mul r7, r6, r8
+    cmp r7, r5 // Compare j with width
     beq iloop
-    
+
     // Load A[i][j] into r6
-    add r6, r2, r5
-    ldr r6, [r6]
-    add r6, r6, r4
-    ldr r6, [r6]
+    ldr r6, [r4, r1]
+    ldr r6, [r6, r5]
 
     // Load B[i][j] into r7
-    add r7, r3, r5
-    ldr r7, [r7]
-    add r7, r7, r4
-    ldr r7, [r7]
+    ldr r7, [r4, r2]
+    ldr r7, [r7, r5]
 
     add r6, r6, r7 // Put sum in r6
 
     // Store address for C[i][j] in r7
-    add r7, r2, r5
-    ldr r7, [r6]
-    add r7, r6, r4
+    ldr r7, [r4, r0]
     
-    str r6, [r7] // Store sum into C
+    str r6, [r7, r5] // Store sum into C
+    add r5, r5, #4
     b jloop
 return:
-    pop {r4, r5, r6, r7, pc} 
+    pop {r4, r5, r6, r7, r8, pc} 
